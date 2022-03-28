31018b2fa1c63a2d549ddb309e582c7b5d82673a
arnaud.coquerelle@gmail.com <arnauddu95>
2022-03-28 12:37:01 +0200

    form 3

---
+++ Lab3/Form3.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Lab3
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.leftListLang = new System.Windows.Forms.ListBox();
+            this.rightListLang = new System.Windows.Forms.ListBox();
+            this.btn_ToRight = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // leftListLang
+            // 
+            this.leftListLang.FormattingEnabled = true;
+            this.leftListLang.Location = new System.Drawing.Point(44, 36);
+            this.leftListLang.Name = "leftListLang";
+            this.leftListLang.Size = new System.Drawing.Size(208, 225);
+            this.leftListLang.TabIndex = 0;
+            // 
+            // rightListLang
+            // 
+            this.rightListLang.FormattingEnabled = true;
+            this.rightListLang.Location = new System.Drawing.Point(536, 36);
+            this.rightListLang.Name = "rightListLang";
+            this.rightListLang.Size = new System.Drawing.Size(208, 225);
+            this.rightListLang.TabIndex = 1;
+            // 
+            // btn_ToRight
+            // 
+            this.btn_ToRight.Location = new System.Drawing.Point(331, 36);
+            this.btn_ToRight.Name = "btn_ToRight";
+            this.btn_ToRight.Size = new System.Drawing.Size(106, 38);
+            this.btn_ToRight.TabIndex = 2;
+            this.btn_ToRight.Text = ">";
+            this.btn_ToRight.UseVisualStyleBackColor = true;
+            this.btn_ToRight.Click += new System.EventHandler(this.btn_ToRight_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(331, 89);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 42);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "<";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(331, 153);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 44);
+            this.button3.TabIndex = 4;
+            this.button3.Text = ">>";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(331, 218);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(106, 43);
+            this.button4.TabIndex = 5;
+            this.button4.Text = ">>";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(44, 306);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(92, 31);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "UP";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(160, 306);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(92, 31);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "DOWN";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(44, 361);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(92, 31);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "A-Z";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(160, 361);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(92, 31);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "Z-A";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(652, 361);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(92, 31);
+            this.button9.TabIndex = 13;
+            this.button9.Text = "Z-A";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(536, 361);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(92, 31);
+            this.button10.TabIndex = 12;
+            this.button10.Text = "A-Z";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(652, 306);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(92, 31);
+            this.button11.TabIndex = 11;
+            this.button11.Text = "DOWN";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(536, 306);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(92, 31);
+            this.button12.TabIndex = 10;
+            this.button12.Text = "UP";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btn_ToRight);
+            this.Controls.Add(this.rightListLang);
+            this.Controls.Add(this.leftListLang);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox leftListLang;
+        private System.Windows.Forms.ListBox rightListLang;
+        private System.Windows.Forms.Button btn_ToRight;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+    }
+}
---
+++ Lab3/Form3.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3
+{
+    public partial class Form3 : Form
+    {
+        List<string> leftListL;
+        List<string> rightListL;
+
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            leftListL = new List<string>();
+            leftListL.Add("Language C");
+            leftListL.Add("Language C#");
+            leftListL.Add("Language Java");
+            leftListL.Add("HTML");
+            leftListL.Add("CSS");
+            leftListL.Add("JavaScript");
+            leftListL.Add("Python");
+            leftListLang.Items.AddRange(leftListL.ToArray());
+            rightListL = new List<string>();
+
+        }
+
+        private void btn_ToRight_Click(object sender, EventArgs e)
+        {
+            int index = leftListLang.SelectedIndex;
+            Console.WriteLine(index);
+            rightListLang.Items.Add(leftListL.ElementAt(index));
+            leftListL.RemoveAt(index);
+            leftListLang.Items.RemoveAt(index);
+
+        }
+    }
+}
